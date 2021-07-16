95eb236e77869040c9f9cc5314536068890d89d8
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-07-16 11:05:30 -0500

    making changes

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -1,158 +1,93 @@
-package com.codeup.adlister.dao;
-
-import com.codeup.adlister.models.Ad;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-
-
-
-<<<<<<< HEAD
-    // may need to implements Ads at a later time - Lex
-
-
-=======
-// may need to implements Ads at a later time - Lex
-
-
-
-public class ListAdsDao {
-
-
-    // may need to implements Ads at a later time - Lex
-
-
->>>>>>> 0d7ed97327849a6dd68ed68889168d40c3f38c7a
-        private List<Ad> ads;
-
-        public List<Ad> all() {
-            if (ads == null) {
-                ads = generateAds();
-            }
-            return ads;
-<<<<<<< HEAD
-        }
-
-        public Long insert(Ad ad) {
-            // make sure we have ads
-            if (ads == null) {
-                ads = generateAds();
-            }
-            // we'll assign an "id" here based on the size of the ads list
-            // really the dao would handle this
-            ad.setId((long) ads.size());
-            ads.add(ad);
-            return ad.getId();
-        }
-
-        private List<Ad> generateAds() {
-            List<Ad> ads = new ArrayList<>();
-            ads.add(new Ad(
-                    1,
-                    1,
-                    "playstation for sale",
-                    "This is a slightly used playstation"
-            ));
-            ads.add(new Ad(
-                    2,
-                    1,
-                    "Super Nintendo",
-                    "Get your game on with this old-school classic!"
-            ));
-            ads.add(new Ad(
-                    3,
-                    2,
-                    "Junior Java Developer Position",
-                    "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
-            ));
-            ads.add(new Ad(
-                    4,
-                    2,
-                    "JavaScript Developer needed",
-                    "Must have strong Java skills"
-            ));
-            return ads;
-        }
-=======
-        }
-
-        public Long insert(Ad ad) {
-            // make sure we have ads
-            if (ads == null) {
-                ads = generateAds();
-            }
-            // we'll assign an "id" here based on the size of the ads list
-            // really the dao would handle this
-            ad.setId((long) ads.size());
-            ads.add(ad);
-            return ad.getId();
-        }
-
-    private List<Ad> generateAds() {
-        List<Ad> ads = new ArrayList<>();
-        ads.add(new Ad(
-                1,
-                1,
-                "playstation for sale",
-                "This is a slightly used playstation"
-        ));
-        ads.add(new Ad(
-                2,
-                1,
-                "Super Nintendo",
-                "Get your game on with this old-school classic!"
-        ));
-        ads.add(new Ad(
-                3,
-                2,
-                "Junior Java Developer Position",
-                "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
-        ));
-        ads.add(new Ad(
-                4,
-                2,
-                "JavaScript Developer needed",
-                "Must have strong Java skills"
-        ));
-        return ads;
-    }
-
->>>>>>> 0d7ed97327849a6dd68ed68889168d40c3f38c7a
-}
-
-}
-
-        private List<Ad> generateAds() {
-            List<Ad> ads = new ArrayList<>();
-            ads.add(new Ad(
-                    1,
-                    1,
-                    "playstation for sale",
-                    "This is a slightly used playstation"
-            ));
-            ads.add(new Ad(
-                    2,
-                    1,
-                    "Super Nintendo",
-                    "Get your game on with this old-school classic!"
-            ));
-            ads.add(new Ad(
-                    3,
-                    2,
-                    "Junior Java Developer Position",
-                    "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
-            ));
-            ads.add(new Ad(
-                    4,
-                    2,
-                    "JavaScript Developer needed",
-                    "Must have strong Java skills"
-            ));
-            return ads;
-        }
-    }
-
-
-
+//package com.codeup.adlister.dao;
+//
+//import com.codeup.adlister.models.Ad;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//
+//
+//
+//
+//    // may need to implements Ads at a later time - Lex
+//
+//
+//// may need to implements Ads at a later time - Lex
+//
+//
+//
+//public class ListAdsDao {
+//
+//
+//    // may need to implements Ads at a later time - Lex
+//
+//
+//    private List<Ad> ads;
+//
+//    public List<Ad> all() {
+//        if (ads == null) {
+//            ads = generateAds();
+//        }
+//        return ads;
+//
+//    }
+//
+//    public Long insert(Ad ad) {
+//        // make sure we have ads
+//        if (ads == null) {
+//            ads = generateAds();
+//        }
+//        // we'll assign an "id" here based on the size of the ads list
+//        // really the dao would handle this
+//        ad.setId((long) ads.size());
+//        ads.add(ad);
+//        return ad.getId();
+//    }
+//
+//    private List<Ad> generateAds() {
+//        List<Ad> ads = new ArrayList<>();
+//        ads.add(new Ad(
+//                1,
+//                1,
+//                "playstation for sale",
+//                "This is a slightly used playstation"
+//        ));
+//        ads.add(new Ad(
+//                2,
+//                1,
+//                "Super Nintendo",
+//                "Get your game on with this old-school classic!"
+//        ));
+//        ads.add(new Ad(
+//                3,
+//                2,
+//                "Junior Java Developer Position",
+//                "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
+//        ));
+//        ads.add(new Ad(
+//                4,
+//                2,
+//                "JavaScript Developer needed",
+//                "Must have strong Java skills"
+//        ));
+//        return ads;
+//    }
+//
+//    public Long insert(Ad ad) {
+//        // make sure we have ads
+//        if (ads == null) {
+//            ads = generateAds();
+//        }
+//        // we'll assign an "id" here based on the size of the ads list
+//        // really the dao would handle this
+//        ad.setId((long) ads.size());
+//        ads.add(ad);
+//        return ad.getId();
+//    }
+//
+//}
+//
+//
+//
+//
