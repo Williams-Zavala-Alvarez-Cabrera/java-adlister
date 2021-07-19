e9ddad6b580fb922c788915a106e933919956791
Lex Zavala <lex.zavala7@gmail.com>
2021-07-18 22:43:45 -0500

    fixed issue with login servlet

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -11,13 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 import java.sql.*;
 
 import static com.codeup.adlister.dao.DaoFactory.getUsersDao;
-
-
-
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -34,63 +30,28 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost : 3306/adlister_db?useSSL=false", "root", "codeup");
-
-            //get data from username and password table using query:
-            Statement stm = con.createStatement();
-            ResultSet rs = stm.executeQuery("SELECT * FROM users WHERE username='" + username + "'AND password='" + password + "'");
-            boolean validAttempt = Password.check(password, user.getPassword());
-            boolean noInput = username.isEmpty()
-                    || password.isEmpty();
-            if (rs.next()) {
-                //
-                response.sendRedirect("/profile");
-            }else if (validAttempt) {
-                request.getSession().setAttribute("user", user);
-                User currentUser = (User) request.getSession().getAttribute("user");
-                String currentUsername = currentUser.getUsername();
-                String email = currentUser.getEmail();
-                request.getSession().setAttribute("username", currentUsername);
-                request.getSession().setAttribute("email", email);
-                if (request.getSession().getAttribute("visitAttempt") =="adsCreate") {
-                    response.sendRedirect("/ads/create");
-                } else {
-                    response.sendRedirect("/profile");
-
-                }
-            }
-            else if(noInput){
-                response.sendRedirect("/repeat_login");
-            }
-
-        } catch (SQLException | ClassNotFoundException throwables) {
-            throwables.printStackTrace();
+        if (user == null) {
+            response.sendRedirect("/login");
+            return;
         }
 
-//        Commented it out in case we might need this again - Alejandro
-//        if (user == null) {
-//            response.sendRedirect("/login");
-//            return;
-//        }
+        boolean validAttempt = Password.check(password, user.getPassword());
 
-//        else if (validAttempt) {
-//            request.getSession().setAttribute("user", user);
-//            User currentUser = (User) request.getSession().getAttribute("user");
-//            String currentUsername = currentUser.getUsername();
-//            String email = currentUser.getEmail();
-//            request.getSession().setAttribute("username", currentUsername);
-//            request.getSession().setAttribute("email", email);
-//            if (request.getSession().getAttribute("visitAttempt") =="adsCreate") {
-//                response.sendRedirect("/ads/create");
-//            } else {
-//                response.sendRedirect("/profile");
-//
-//            }
-//        } else {
-//                response.sendRedirect("/login");
-//            }
-//        }
+        if (validAttempt) {
+            request.getSession().setAttribute("user", user);
+            User currentUser = (User) request.getSession().getAttribute("user");
+            String currentUsername = currentUser.getUsername();
+            String email = currentUser.getEmail();
+            request.getSession().setAttribute("username", currentUsername);
+            request.getSession().setAttribute("email", email);
+            if (request.getSession().getAttribute("visitAttempt") =="adsCreate") {
+                response.sendRedirect("/ads/create");
+            } else {
+                response.sendRedirect("/profile");
+
+            }
+        } else {
+            response.sendRedirect("/login");
+        }
     }
 }
